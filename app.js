a10cba13fa3db4deeffedf03a66b9b847687ccaf
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2024-03-15 19:00:47 +0530

    Added compression and helmet middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +12,7 @@ const categoriesRouter = require('./routes/categories');
 
 var app = express();
 const mongoose = require('mongoose');
+const compression = require('compression');
 mongoose.set('strictQuery', false);
 const dev_db_url = "mongodb+srv://bhaskarbordoloi:OzpLy2lvJEw3LR71@cluster0.llcewnk.mongodb.net/inventory_data?retryWrites=true&w=majority&appName=Cluster0"
 const mongoDB =  process.env.MONGO_URI || dev_db_url;
@@ -25,6 +27,8 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compression());
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
